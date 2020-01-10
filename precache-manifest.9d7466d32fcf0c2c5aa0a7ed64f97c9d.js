549778647ad5e6549ed9067a899171107b726bdd
Milind Patil <milindpatil716@gmail.com>
2020-01-10 21:24:58 +0530

    Updates

---
+++ precache-manifest.9d7466d32fcf0c2c5aa0a7ed64f97c9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aac578896472487eabf25d4a21696ce",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "63ad613b632a6c4ea113",
+    "url": "/monsters-rolodex/static/css/main.d9afc66f.chunk.css"
+  },
+  {
+    "revision": "854691ce812d4fc7a13b",
+    "url": "/monsters-rolodex/static/js/2.62808603.chunk.js"
+  },
+  {
+    "revision": "63ad613b632a6c4ea113",
+    "url": "/monsters-rolodex/static/js/main.21a17972.chunk.js"
+  },
+  {
+    "revision": "cff99cc41fad18162bb8",
+    "url": "/monsters-rolodex/static/js/runtime-main.8b196bbe.js"
+  }
+]);
